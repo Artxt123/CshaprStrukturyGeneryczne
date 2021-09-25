e627b8c14080c92d4b674b7a2d8458193cbd6e2d
Artur Oczkowski <alfaix@o2.pl>
2021-09-25 15:52:01 +0200

    1_5 UnitTests

---
+++ CsharpStrukturyGenerycznee/TypyGeneryczneTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TypyGeneryczneTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TypyGeneryczneTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0ea471d1-2b4d-47ef-b82b-c70c5f16e7e6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CsharpStrukturyGenerycznee/TypyGeneryczneTest/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using _1_TypyGeneryczne;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _1_TypyGeneryczneTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NowaKolejkaJestPusta()
+        {
+            var kolejka = new KolejkaKolowa();
+            Assert.IsTrue(kolejka.JestPusty);
+        }
+
+        [TestMethod]
+        public void KolejkaTrzyElementowaJestPelnaPoTrzechZapisach()
+        {
+            var kolejka = new KolejkaKolowa(pojemnosc: 3);
+            kolejka.Zapisz(2.3);
+            kolejka.Zapisz(3.6);
+            kolejka.Zapisz(7.1);
+
+            Assert.IsTrue(kolejka.JestPelny);
+        }
+
+        [TestMethod]
+        public void FIFO() //PierwszyWchodziPierwszyWychodzi
+        {
+            var kolejka = new KolejkaKolowa(pojemnosc: 3);
+            var wartosc1 = 2.2;
+            var wartosc2 = 3.6;
+
+            kolejka.Zapisz(wartosc1);
+            kolejka.Zapisz(wartosc2);
+
+            Assert.AreEqual(wartosc1, kolejka.Czytaj());
+            Assert.AreEqual(wartosc2, kolejka.Czytaj());
+            Assert.IsTrue(kolejka.JestPusty);
+        }
+        [TestMethod]
+        public void NadpisujeGdyJestWiekszaNizPojemnosc()
+        {
+            var kolejka = new KolejkaKolowa(pojemnosc: 3);
+            var wartosci = new[] { 2.3, 3.4, 4.4, 5.7, 8.8, 9.9 };
+
+            foreach (var wartosc in wartosci)
+            {
+                kolejka.Zapisz(wartosc);
+            }
+
+            Assert.IsTrue(kolejka.JestPelny);
+            Assert.AreEqual(wartosci[3], kolejka.Czytaj());
+            Assert.AreEqual(wartosci[4], kolejka.Czytaj());
+            Assert.AreEqual(wartosci[5], kolejka.Czytaj());
+            Assert.IsTrue(kolejka.JestPusty);
+        }
+    }
+}
