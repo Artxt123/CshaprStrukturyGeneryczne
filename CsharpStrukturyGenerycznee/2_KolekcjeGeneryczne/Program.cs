bec3d68d4a91b512b893c033ecd15d521e1aed9b
Artur Oczkowski <alfaix@o2.pl>
2021-10-10 23:26:12 +0200

    2_7 HashSet

--- CsharpStrukturyGenerycznee/2_KolekcjeGeneryczne/Program.cs
+++ CsharpStrukturyGenerycznee/2_KolekcjeGeneryczne/Program.cs
@@ -10,32 +10,10 @@ namespace _2_KolekcjeGeneryczne
     {
         static void Main(string[] args)
         {
-            Queue<Pracownik> kolejka = new Queue<Pracownik>();
-            kolejka.Enqueue(new Pracownik { Imie = "Michał", Nazwisko = "Andrzejak" });
-            kolejka.Enqueue(new Pracownik { Imie = "Katarzyna", Nazwisko = "Miła" });
-            kolejka.Enqueue(new Pracownik { Imie = "Marcin", Nazwisko = "Kowalski" });
-            kolejka.Enqueue(new Pracownik { Imie = "Tomasz", Nazwisko = "Sęk" });
+            //Kolejka();
+            //Stos();
 
-            Console.WriteLine("Kolejka:");
-
-            while (kolejka.Count > 0)
-            {
-                var pracownik = kolejka.Dequeue();
-                Console.WriteLine($"{pracownik.Imie} {pracownik.Nazwisko}");
-            }
-
-            Stack<Pracownik> stos = new Stack<Pracownik>();
-            stos.Push(new Pracownik { Imie = "Michał", Nazwisko = "Andrzejak" });
-            stos.Push(new Pracownik { Imie = "Katarzyna", Nazwisko = "Miła" });
-            stos.Push(new Pracownik { Imie = "Marcin", Nazwisko = "Kowalski" });
-            stos.Push(new Pracownik { Imie = "Tomasz", Nazwisko = "Sęk" });
-
-            Console.WriteLine($"\nStos:");
-            while (stos.Count > 0)
-            {
-                var pracownik = stos.Pop();
-                Console.WriteLine($"{pracownik.Imie} {pracownik.Nazwisko}");
-            }
+            HashSet();
 
 
 
@@ -60,6 +38,52 @@ namespace _2_KolekcjeGeneryczne
             //Array.Resize(ref pracownicy, 10);
             //pracownicy[9] = new Pracownik { Imie = "Janusz", Nazwisko = "Kopeć" };
 
+        }
+
+        private static void HashSet()
+        {
+            HashSet<Pracownik> set = new HashSet<Pracownik>();
+            var pracownik = new Pracownik { Imie = "Michał", Nazwisko = "Andrzejak" };
+            set.Add(pracownik);
+            set.Add(pracownik);
+
+            foreach (var item in set)
+            {
+                Console.WriteLine($"{item.Imie} {item.Nazwisko}");
+            }
+        }
+
+        private static void Kolejka()
+        {
+            Queue<Pracownik> kolejka = new Queue<Pracownik>();
+            kolejka.Enqueue(new Pracownik { Imie = "Michał", Nazwisko = "Andrzejak" });
+            kolejka.Enqueue(new Pracownik { Imie = "Katarzyna", Nazwisko = "Miła" });
+            kolejka.Enqueue(new Pracownik { Imie = "Marcin", Nazwisko = "Kowalski" });
+            kolejka.Enqueue(new Pracownik { Imie = "Tomasz", Nazwisko = "Sęk" });
+
+            Console.WriteLine("Kolejka:");
+
+            while (kolejka.Count > 0)
+            {
+                var pracownik = kolejka.Dequeue();
+                Console.WriteLine($"{pracownik.Imie} {pracownik.Nazwisko}");
+            }
+        }
+
+        private static void Stos()
+        {
+            Stack<Pracownik> stos = new Stack<Pracownik>();
+            stos.Push(new Pracownik { Imie = "Michał", Nazwisko = "Andrzejak" });
+            stos.Push(new Pracownik { Imie = "Katarzyna", Nazwisko = "Miła" });
+            stos.Push(new Pracownik { Imie = "Marcin", Nazwisko = "Kowalski" });
+            stos.Push(new Pracownik { Imie = "Tomasz", Nazwisko = "Sęk" });
+
+            Console.WriteLine($"\nStos:");
+            while (stos.Count > 0)
+            {
+                var pracownik = stos.Pop();
+                Console.WriteLine($"{pracownik.Imie} {pracownik.Nazwisko}");
+            }
         }
     }
 }
