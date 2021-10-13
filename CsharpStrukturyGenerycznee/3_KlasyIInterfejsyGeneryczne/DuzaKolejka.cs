9a79fcc9e240ad24e6ddff4e5ecda203fcab3a9e
Artur Oczkowski <alfaix@o2.pl>
2021-10-13 18:18:24 +0200

    3_5 IEnumerable

--- CsharpStrukturyGenerycznee/3_KlasyIInterfejsyGeneryczne/DuzaKolejka.cs
+++ CsharpStrukturyGenerycznee/3_KlasyIInterfejsyGeneryczne/DuzaKolejka.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace _3_KlasyIInterfejsyGeneryczne
 {
@@ -29,6 +30,24 @@ namespace _3_KlasyIInterfejsyGeneryczne
         public virtual void Zapisz(T wartosc)
         {
             kolejka.Enqueue(wartosc);
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            //Aby to działało można zrobić tak:
+            //return kolejka.GetEnumerator();
+
+            //Można też zrobić własnego foreach'a w którym będzie jakieś filtrowanie i potem będzie zwracał dane:
+            foreach (var item in kolejka)
+            {
+                //mijesce na jakieś filtoranie danych
+                //......
+
+                yield return item; //leniwie zwracamy item, yield pozwala na zwracanie wartości pojedynczo; inaczej nie zadziała
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
