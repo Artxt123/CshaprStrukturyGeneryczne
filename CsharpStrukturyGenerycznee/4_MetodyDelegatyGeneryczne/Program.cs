46c96eb8004876684d2624f970c4cbe2729df0a8
Artur Oczkowski <alfaix@o2.pl>
2021-10-14 23:40:34 +0200

    4_5 Func

--- CsharpStrukturyGenerycznee/4_MetodyDelegatyGeneryczne/Program.cs
+++ CsharpStrukturyGenerycznee/4_MetodyDelegatyGeneryczne/Program.cs
@@ -11,6 +11,7 @@ namespace _4_MetodyDelegatyGeneryczne
         static void Main(string[] args)
         {
             var kolejka = new KolejkaKolowa<double>();
+            #region Action
 
             //Action to delegat, który zawsze zwraca void, może przyjmować od 0 do 16 argumentów
             //Przypisujemy do drukuj metodę anonimową, która przyjumuje double dane i wypisuje je na ekran
@@ -20,10 +21,23 @@ namespace _4_MetodyDelegatyGeneryczne
             //Action<double> drukuj = d => Console.WriteLine(d); //d jest tym double i wypisujemy to na ekranie
 
             //Inne rzeczy z wykorzystanie Action:
-            Action<double> drukuj = x => Console.WriteLine(x);
-            drukuj(7.89);
-            Action<int, int, int> dodajWypisz = (a, b, c) => Console.WriteLine(a + b + c);
-            dodajWypisz(1, 2, 5);
+            //Action<double> drukuj = x => Console.WriteLine(x);
+            //drukuj(7.89);
+            //Action<int, int, int> dodajWypisz = (a, b, c) => Console.WriteLine(a + b + c);
+            //dodajWypisz(1, 2, 5);
+            #endregion
+
+            Action<double> drukuj = d => Console.WriteLine(d);
+
+            //Func zawsze zwraca coś zwraca, ten typ określamy na samym końcu przy podawaniu parametrów
+
+            //Tutaj przyjumje double i zwraca double:
+            Func<double, double> potegowanie = d => d * d;
+            //Tutaj przyjmuje dwa double i zwraca double:
+            Func<double, double, double> dodaj = (x, y) => x + y;
+
+            drukuj(potegowanie(5.5));
+            drukuj(dodaj(3.4, 1.11));
 
             WprowadzanieDanych(kolejka);
 
